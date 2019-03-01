9acd88afa2f21eb1ff22be51840b53204143ce9e
Angel Lando <angel@mbp-de-angel-2.einet.ad.eivd.ch>
2019-03-01 10:58:53 +0100

    SVG interaction

--- 20190301-manipulation-dom/exercice_map/PriscaHuguenot.js
+++ 20190301-manipulation-dom/exercice_map/PriscaHuguenot.js
@@ -5,8 +5,30 @@ const data = [
   { nom: 'Pierrette', note: 52 },
 ]
 
+const data = [
+  { nom: 'Paul', note: 34 },
+  { nom: 'Pauline', note: 98 },
+  { nom: 'Pierre', note: 74 },
+  { nom: 'Pierrette', note: 52 },
+]
+
+function getName(item) {
+  var name = [item.nom];
+  return name;
+}
+
+function getNote(item) {
+  var note = [item.note];
+  return note;
+}
+
+function getNoteSur20(item) {
+  var noteSur20 = [item.note];
+  return noteSur20/5;
+}
+
 console.log({
-  noms: data.map(),
-  notes: data.map(),
-  notesSur20: data.map(),
+  noms: data.map(getName),
+  notes: data.map(getNote),
+  notesSur20: data.map(getNoteSur20),
 })
