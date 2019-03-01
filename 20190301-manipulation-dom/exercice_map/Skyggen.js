024cfea5a454054272764d284c7d6c3bb71061fa
Adrien Ciampone <39824607+Skyggen@users.noreply.github.com>
2019-03-01 08:55:45 +0100

    Update Skyggen.js

--- 20190301-manipulation-dom/exercice_map/Skyggen.js
+++ 20190301-manipulation-dom/exercice_map/Skyggen.js
@@ -6,7 +6,7 @@ const data = [
 ]
 
 console.log({
-  noms: data.map(),
-  notes: data.map(),
-  notesSur20: data.map(),
+  noms: data.map(nom),
+  notes: data.map(note),
+  notesSur20: data.map(note/20),
 })
