6cc0d30993161340fcb429183095f6cdd5e9e60a
Anders <idris.maps@gmx.ch>
2019-03-08 10:11:06 +0100

    Add exercice_liste_2

---
+++ 20190308-intro-data/exercice_listes_2/Lowgic.js
@@ -0,0 +1,15 @@
+const villes = [
+  { nom: 'Lausanne', population: 138905 },
+  { nom: 'Yverdon', population: 30143 },
+  { nom: 'Montreux', population: 26574 },
+  { nom: 'Renens', population: 21036 },
+  { nom: 'Nyon', population: 20533 },
+  { nom: 'Vevey', population: 19827 },
+]
+
+module.exports = {
+  populationDesVilles: villes,
+  villesDeMoinsDe30000Habitants: villes,
+  nombreDHabitantsDeNyon: villes,
+  sommeDuNombreDHabitants: villes,
+}
