5c997255460c9224bf6673ae0e3691ae9c12981d
Audric Dubuis <audric.dubuis@heig-vd.ch>
2019-03-08 10:25:58 +0100

    Ajout de l'exercice

--- 20190308-intro-data/exercice_listes/Lowgic.js
+++ 20190308-intro-data/exercice_listes/Lowgic.js
@@ -7,9 +7,11 @@ const villes = [
   { nom: 'Vevey', population: 19827 },
 ]
 
+const somme = (result, d) => result + d.population;
+
 module.exports = {
-  nomsDesVilles: villes,
-  villesDePlusDe30000Habitants: villes,
-  nombreDHabitantsDYverdon: villes,
-  sommeDuNombreDHabitants: villes,
+  nomsDesVilles: villes.map(d => d.nom),
+  villesDePlusDe30000Habitants: villes.filter(d => d.population > 30000),
+  nombreDHabitantsDYverdon: villes.find(d => d.nom ==='Yverdon').population,
+  sommeDuNombreDHabitants: villes.reduce(somme,0),
 }
