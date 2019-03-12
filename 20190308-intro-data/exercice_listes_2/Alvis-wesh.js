0f7ec84ad159c7e82203f52c0e477217f35e04c6
Alvis-wesh <44233811+Alvis-wesh@users.noreply.github.com>
2019-03-12 20:00:26 +0100

    ajout des fonctions

--- 20190308-intro-data/exercice_listes_2/Alvis-wesh.js
+++ 20190308-intro-data/exercice_listes_2/Alvis-wesh.js
@@ -1,3 +1,5 @@
+// création de la liste 
+
 const villes = [
   { nom: 'Lausanne', population: 138905 },
   { nom: 'Yverdon', population: 30143 },
@@ -7,9 +9,12 @@ const villes = [
   { nom: 'Vevey', population: 19827 },
 ]
 
+const total = (result, d) => result + d.population;
+
 module.exports = {
-  populationDesVilles: villes,
-  villesDeMoinsDe30000Habitants: villes,
-  nombreDHabitantsDeNyon: villes,
-  sommeDuNombreDHabitants: villes,
+  nomsDesVilles: villes.map(d => d.population), // fonction .map
+  villesDePlusDe30000Habitants: villes.filter(d => d.population < 30000), // fonction .filter
+  nombreDHabitantsDYverdon: villes.find(d => d.nom == 'Nyon').population, // fonction .find pour Nyon
+  sommeDuNombreDHabitants: villes.reduce(total, 0), // fonction .reduce 
 }
+
