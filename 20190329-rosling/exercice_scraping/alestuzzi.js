e519ca0d0e7afcb3b4d0ad6fe407d1ca6fe5bef9
alestuzzi <44233644+alestuzzi@users.noreply.github.com>
2019-04-04 20:55:55 +0200

    lignes inutiles dans l'en-tête

--- 20190329-rosling/exercice_scraping/alestuzzi.js
+++ 20190329-rosling/exercice_scraping/alestuzzi.js
@@ -1,9 +1,6 @@
 const cheerio = require('cheerio')
 const R = require('ramda')
 const fs = require('fs')
-const $ = require('cheerio');
-const R = require('ramda');
-const fs = require('fs');
 
 
 const page = fs.readFileSync('laptops.html', 'utf-8')
