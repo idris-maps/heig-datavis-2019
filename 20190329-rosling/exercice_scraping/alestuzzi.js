39061e2952cf53f4c434588ea5ece5c57cf92f7a
Adrien Lestuzzi <adrien.lestuzzi@heig-vd.ch>
2019-04-05 08:34:30 +0200

    ajout de test

--- 20190329-rosling/exercice_scraping/alestuzzi.js
+++ 20190329-rosling/exercice_scraping/alestuzzi.js
@@ -2,4 +2,13 @@ const cheerio = require('cheerio')
 const R = require('ramda')
 const fs = require('fs')
 
-const page = fs.readFileSync('laptops.html', 'utf-8')
+const page = fs.readFileSync('laptops.html', 'utf-8');
+//var cofvefe = page.getElementById("wrapper");
+//var res = cofvefe.getElementsByTagName('thumbnail');
+
+fetch(page)
+    .then(page.prop("wrapper"))
+    .then(page.val("container test site"))
+    .catch(console.log);
+
+console.log(res);
