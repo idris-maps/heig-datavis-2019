26bbeb815cc6152674a85d620dc5aa0ebe672e62
Steve Maibach <steve.maibach@heig-vd.ch>
2019-04-05 14:30:37 +0200

    Ex ramda

--- 20190405-carto/exercice_ramda/vesti1066.js
+++ 20190405-carto/exercice_ramda/vesti1066.js
@@ -5,3 +5,35 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com'
 
 const URL_POSTS = `${BASE_URL}/posts`
 const URL_USERS = `${BASE_URL}/users`
+
+let download = url => fetch(url)
+			.then(res => res.json())
+//			.then(console.log)
+
+let list = ([users, posts]) => {
+
+	
+
+	return users.map(user => {
+
+		return {
+
+		    nom_utilisateur: R.prop('name', user),
+		    ville: R.prop('address', user).city,
+		    nom_companie: R.prop('company', user).name,
+		    titres_posts: posts.filter(R.propEq('userId', user.id))
+		     			 .map(R.prop('title'))
+		}
+
+	})
+
+} 
+
+Promise.all([
+
+	download(URL_USERS),
+	download(URL_POSTS),
+
+	])
+	.then(list)
+	.then(console.log);
