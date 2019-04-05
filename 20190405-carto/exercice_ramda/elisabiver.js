6c5c01122131706c0fcd7e27d753fe4051d2caa4
elisabiver <44230413+elisabiver@users.noreply.github.com>
2019-04-05 15:39:51 +0200

    function added correction

--- 20190405-carto/exercice_ramda/elisabiver.js
+++ 20190405-carto/exercice_ramda/elisabiver.js
@@ -5,3 +5,25 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com'
 
 const URL_POSTS = `${BASE_URL}/posts`
 const URL_USERS = `${BASE_URL}/users`
+
+const telecharger = url =>
+fetch(url).then(r => r.json());
+
+const maFonction = ([posts, users]) => {
+    return users.map(user => {
+        return {
+            nom_utilisateur: R.prop('username', user),
+            ville: R.path(['adress', 'city'], user),
+            nom_companie: R.path(['company', 'name'], user),
+            titres_posts: posts.filter(R.propEq('userId', user.id)).map(R.prop('title'))
+        }
+    })
+}
+
+Promise.all([
+    telecharger(URL_POSTS),
+    telecharger(URL_USERS)
+])
+.then(maFonction)
+.then(console.log)
+.catch(console.log)
