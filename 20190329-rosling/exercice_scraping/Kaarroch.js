47c1e64f52a654f0740dfe459c87c744455bcafb
Karim <karim.rochat@heig-vd.ch>
2019-04-05 07:07:54 +0200

    Exercice complété

--- 20190329-rosling/exercice_scraping/Kaarroch.js
+++ 20190329-rosling/exercice_scraping/Kaarroch.js
@@ -7,7 +7,7 @@ const page = fs.readFileSync('laptops.html', 'utf-8')
 
 /* Le corps du code */
 
-const thumbnail = cheerio('.thumbnail', page) //J'utilise cheerio pour directement récupérer toutes les classes thumbnails, qui elles contiennent les noms, les prix et les étoiles que je veux pour chaque produit.
+const thumbnails = cheerio('.thumbnail', page) //J'utilise cheerio pour directement récupérer toutes les classes thumbnails, qui elles contiennent les noms, les prix et les étoiles que je veux pour chaque produit.
 
 //Je récupère l'élément HTML précis qui représente chaque nom, prix et étoiles 
 const getNom = R.path(['children', 3, 'children', 3, 'children', 1, 'attribs', 'title'])
@@ -15,10 +15,9 @@ const getPrix = R.path(['children', 3, 'children', 1, 'children', 0, 'data'])
 const getEtoiles = R.path(['children', 5, 'children', 3, 'attribs', 'data-rating'])
 
 //Je fais le tour de toute ma liste thumbnail qui contient la fiche de chaque objet et j'utilise Array.from pour transformer les objets retournés en tableaux 
-const result = Array.from(thumbnail).map(d => ({
-  produit:getNom(d), //J'affiche le produit
-  prix: getPrix(d), //puis le prix
-  etoiles: getEtoiles(d) //puis le nombre d'étoiles
+const liste = Array.from(thumbnails).map(ordinateur => ({
+  produit:getNom(ordinateur), //J'affiche le produit
+  prix: getPrix(ordinateur), //puis le prix
+  etoiles: getEtoiles(ordinateur) //puis le nombre d'étoiles
 }))
-
-console.log(result);
+console.log(liste);
