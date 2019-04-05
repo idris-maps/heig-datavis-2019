665ec84d963c7e6cd07a51b39808bbd92a8a6a5a
Anders <idris.maps@gmx.ch>
2019-04-05 11:14:56 +0200

    Add files for exercice ramda

---
+++ 20190405-carto/exercice_ramda/alestuzzi.js
@@ -0,0 +1,7 @@
+const fetch = require('node-fetch')
+const R = require('ramda')
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+const URL_POSTS = `${BASE_URL}/posts`
+const URL_USERS = `${BASE_URL}/users`
