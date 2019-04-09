5c1bee0ad72d524047e73e07eef76129805212aa
Gaëtan Haefeli <gagah98@gmail.com>
2019-04-09 17:13:34 +0200

    exo ramda

--- 20190405-carto/exercice_ramda/Gagah98.js
+++ 20190405-carto/exercice_ramda/Gagah98.js
@@ -5,3 +5,26 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com'
 
 const URL_POSTS = `${BASE_URL}/posts`
 const URL_USERS = `${BASE_URL}/users`
+
+const telecharger = url =>
+    fetch(url).then(r => r.json())
+
+const format = ([posts, users]) => {
+    return users.map(user => ({
+        
+            nom_utilisateur: R.prop('username', user),
+            ville: R.path(['address', 'city'], user),
+            nom_companie: R.path(['company', 'name'], user),
+            titres: posts.filter(R.propEq('userId', user.id)) //sans ramda (post=> post.userId === user.id)
+                .map(post => post.title)
+        
+    }))
+}
+
+Promise.all([
+    telecharger(URL_POSTS),
+    telecharger(URL_USERS)
+])
+    .then(format)
+    .then(console.log)
+    .catch(console.log)
