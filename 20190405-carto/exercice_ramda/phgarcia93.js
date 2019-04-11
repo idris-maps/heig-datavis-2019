495597628ab81d487eef89de7962a43302ede331
Pedro Garcia <phgarcia93@gmail.com>
2019-04-11 16:36:32 +0200

    update exercice ramda

--- 20190405-carto/exercice_ramda/phgarcia93.js
+++ 20190405-carto/exercice_ramda/phgarcia93.js
@@ -5,3 +5,15 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com'
 
 const URL_POSTS = `${BASE_URL}/posts`
 const URL_USERS = `${BASE_URL}/users`
+
+const download = url => fetch(url).then(r => r.json());
+const list = ([posts, users]) => {
+	return users.map(user => {
+		return {
+			nom_utilisateur: R.prop("name", user),
+			ville: R.prop("address", user).city,
+			nom_company: R.prop("company", user).name,
+			titres_posts: posts.filter(R.propEq('userId', user.id)).map(R.prop("title"))
+		}
+	})
+}
