ba72e3575b0b43792a3d98af8baef89beedc6115
Anders <idris.maps@gmx.ch>
2019-04-12 13:15:13 +0200

    Add correction exercice ramda

--- 20190405-carto/exercice_ramda/template.js
+++ 20190405-carto/exercice_ramda/template.js
@@ -5,3 +5,23 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com'
 
 const URL_POSTS = `${BASE_URL}/posts`
 const URL_USERS = `${BASE_URL}/users`
+
+const download = url => fetch(url).then(r => r.json())
+
+const format = ([posts, users]) =>
+  users.map(user => ({
+    nom_utilisateur: R.prop('username', user),
+    ville: R.path(['address', 'city'], user),
+    nom_companie: R.path(['company', 'name'], user),
+    titres_posts: posts
+      .filter(R.propEq('userId', user.id))
+      .map(R.prop('title')),
+  }))
+
+Promise.all([
+  download(URL_POSTS),
+  download(URL_USERS),
+])
+  .then(format)
+  .then(console.log)
+  .catch(console.log)
