87890cd219ce2a7697933377c9c6c162bc53554f
Gioianac <joy.oppliger@heig-vd.ch>
2019-04-16 14:25:16 +0200

    try to add exo_map.js because didn't work last week

---
+++ 20190222-intro/batons/lucienP.md
@@ -0,0 +1,6 @@
+ # les calculs à faire pour créer un graphique en bâtons.
+ 
+ * Définir la surface de notre graphique en baton en y et en x
+ * Mettre à l'echelle les données reçues par rapport a la taille des batons respectifs et la taille maximale du graphe
+ * Calculer les espacements entre les batons. 
+ * Utiliser les méthodes de tri des listes comme .map()
--- 20190329-rosling/exercice_scraping/Gagah98.js
+++ 20190329-rosling/exercice_scraping/Gagah98.js
@@ -3,3 +3,18 @@ const R = require('ramda')
 const fs = require('fs')
 
 const page = fs.readFileSync('laptops.html', 'utf-8')
+
+
+const thumbnail = cheerio('.thumbnail', page)
+
+const getPrice = R.path(['children', 3, 'children', 1, 'children', 0, 'data']) 
+const getRating = R.path(['children', 5, 'children', 3, 'attribs', 'data-rating'])
+const getTitle = R.path(['children', 3, 'children', 3, 'children', 1, 'attribs', 'title'])
+
+const result = Array.from(thumbnail).map(d => ({
+    title:getTitle(d),
+    price:getPrice(d),
+    Rating:getRating(d),
+  })) 
+
+  console.log(result);
