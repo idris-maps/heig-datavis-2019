70ddaea82437c639fe190ae08f6a52cafc57be11
Anders <idris.maps@gmx.ch>
2019-05-01 07:45:04 +0200

    Add pr s by hand

--- 20190405-carto/exercice_ramda/Gioianac.js
+++ 20190405-carto/exercice_ramda/Gioianac.js
@@ -1,7 +1,31 @@
 const fetch = require('node-fetch')
 const R = require('ramda')
 
-const BASE_URL = 'https://jsonplaceholder.typicode.com'
 
+const BASE_URL = 'https://jsonplaceholder.typicode.com'
 const URL_POSTS = `${BASE_URL}/posts`
 const URL_USERS = `${BASE_URL}/users`
+
+
+const telecharger = url => 
+    fetch(url).then(r => r.json())
+
+
+const maFonction = ([posts, users]) => {
+    return users.map(user => {
+        return {
+            nom_utilisateur: 'Machin',
+            ville: 'Truc',
+            nom_companie: 'Bidule',
+            titres_posts: posts.filter(R.propEq('userId', user.id))
+        }
+    })
+}
+
+Promise.all([
+    telecharger(URL_POSTS),
+    telecharger(URL_USERS),
+])
+    .then(maFonction)
+    .then(console.log)
+    .catch(console.log)
